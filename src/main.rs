c5e30f00a648dd6315b4a6ba24bdda96f4d1671f
Ross <xkjq@ymail.com>
2024-05-14 18:40:36 +0100

    show dialogy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use nng::{Protocol, Socket, Error};
 use std::process::Command;
+use rfd::MessageDialog;
 
 fn main() {
     // Get the command line argument
@@ -9,30 +10,49 @@ fn main() {
     let socket = Socket::new(Protocol::Req0).expect("Failed to create socket");
 
     // Connect to the NNG server
-    //socket.dial("tcp://localhost:5555").expect("Failed to connect to server");
     match socket.dial("tcp://localhost:5555") {
         Ok(_) => {
             println!("Connected to server");
         }
         Err(e) => {
             println!("Failed to connect to server: {:?}", e);
-            let output = Command::new("ls")
-                .arg("-l")
-                .output()
-                .expect("Failed to execute command");
-            if output.status.success() {
-                let s = String::from_utf8_lossy(&output.stdout);
-                println!("Command output: {}", s);
-            } else {
-                let s = String::from_utf8_lossy(&output.stderr);
-                println!("Command failed, stderr: {}", s);
+            let choice = MessageDialog::new()
+                .set_title("CRIS Tools not running")
+                .set_description("For advanced voice commands to function CRIS Tools needs to be running.\nDo you want to launch it?")
+                .set_buttons(rfd::MessageButtons::YesNo)
+                .show();
+
+            match choice {
+                rfd::MessageDialogResult::Yes => {
+                    println!("User chose Yes");
+                    let output = Command::new(r"T:\rad-tools\cris_tools.exe")
+                        .spawn();
+
+                    match output {
+                        Ok(_) => {
+                            println!("CRIS Tools launched successfully");
+                        }
+                        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+                            // Handle the NotFound error here
+                            println!("CRIS Tools not found");
+                            eprintln!("Failed to launch CRIS Tools: {:?}", e);
+                        }
+                        Err(e) => {
+                            eprintln!("Failed to launch CRIS Tools: {:?}", e);
+                        }
+                    }
+                },
+                rfd::MessageDialogResult::No => println!("User chose No"),
+                _ => println!("User closed or cancelled the dialog box"),
             }
+
             std::process::exit(1);
         }
     }
 
     // Send the argument to the server
-    socket.send(arg.as_bytes()).expect("Failed to send message");
+    let message = "run/".to_string() + &arg;
+    socket.send(message.as_bytes()).expect("Failed to send message");
 
     // Receive the response from the server
     match socket.recv() {
