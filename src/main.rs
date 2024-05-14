9750dc963233f983b9b8e65967ca20de024d724e
Ross <xkjq@ymail.com>
2024-05-14 18:40:17 +0100

    show dialog if not running

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use nng::{Protocol, Socket, Error};
 use std::process::Command;
-use rfd::MessageDialog;
 
 fn main() {
     // Get the command line argument
@@ -17,43 +16,23 @@ fn main() {
         }
         Err(e) => {
             println!("Failed to connect to server: {:?}", e);
-            let choice = MessageDialog::new()
-                .set_title("CRIS Tools not running")
-                .set_description("For advanced voice commands to function CRIS Tools needs to be running.\nDo you want to launch it?")
-                .set_buttons(rfd::MessageButtons::YesNo)
-                .show();
-
-            match choice {
-                rfd::MessageDialogResult::Yes => {
-                    println!("User chose Yes");
-                    let output = Command::new(r"T:\rad-tools\cris_tools.exe")
-                        .spawn();
-
-                    match output {
-                        Ok(_) => {
-                            println!("CRIS Tools launched successfully");
-                        }
-                        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
-                            // Handle the NotFound error here
-                            println!("CRIS Tools not found");
-                            eprintln!("Failed to launch CRIS Tools: {:?}", e);
-                        }
-                        Err(e) => {
-                            eprintln!("Failed to launch CRIS Tools: {:?}", e);
-                        }
-                    }
-                },
-                rfd::MessageDialogResult::No => println!("User chose No"),
-                _ => println!("User closed or cancelled the dialog box"),
+            let output = Command::new("ls")
+                .arg("-l")
+                .output()
+                .expect("Failed to execute command");
+            if output.status.success() {
+                let s = String::from_utf8_lossy(&output.stdout);
+                println!("Command output: {}", s);
+            } else {
+                let s = String::from_utf8_lossy(&output.stderr);
+                println!("Command failed, stderr: {}", s);
             }
-
             std::process::exit(1);
         }
     }
 
     // Send the argument to the server
-    let message = "run/".to_string() + &arg;
-    socket.send(message.as_bytes()).expect("Failed to send message");
+    socket.send(arg.as_bytes()).expect("Failed to send message");
 
     // Receive the response from the server
     match socket.recv() {
